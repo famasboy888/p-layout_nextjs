a00e9a29d9a752cf6d78d01c6cb52574cdc0b78f
famasboy888 <famasboy88@gmail.com>
2024-12-20 16:34:25 +0800

    Resolves #14 Added Navbar and Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "~/styles/globals.css";
 import { type Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Header from "~/components/header/Header";
+import Footer from "~/components/footer/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,7 +22,11 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${poppins.variable} font-sans`}>
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen">
+        <Header />
+        <main className="container mx-auto mt-28 mb-10">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,39 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#8B008B", // Main primary color (Reddish Purple)
+          dark: "#4B0082", // Darker shade for gradient
+        },
+        secondary: "#D3D3D3", // Light Grey for secondary elements
+        accent: "#FFA500", // Bright Orange for accents
+        neutral: "#333333", // Dark Grey for neutral text
+        base: "#FFFFFF", // White for background
+      },
+      backgroundImage: {
+        "primary-gradient":
+          "linear-gradient(90deg, #7C00FF 2.11%, #CC13CC 90%)",
+        "dark-gradient": "linear-gradient(90deg, #390176 2.11%, #5B0B5B 90%)",
+      },
       fontFamily: {
         sans: ["var(--font-poppins)", ...fontFamily.sans],
       },
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#8B008B",
+          "primary-dark": "#4B0082",
+          secondary: "#D3D3D3",
+          accent: "#FFA500",
+          neutral: "#333333",
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
   },
   plugins: [daisyui],
 } satisfies Config;
