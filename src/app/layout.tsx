72b563c31651e2cd09a06f36a7c3bafd46903476
famasboy888 <famasboy88@gmail.com>
2024-12-20 13:22:04 +0800

    Resolves #12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import "~/styles/globals.css";
 import { type Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "P-Layout | Events Coordination & Layouts App",
@@ -11,7 +19,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable} font-sans`}>
       <body>{children}</body>
     </html>
   );
