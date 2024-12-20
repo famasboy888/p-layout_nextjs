fd6da11b1672895b24711e71601f34299844d98c
famasboy888 <famasboy88@gmail.com>
2024-12-20 16:34:25 +0800

    Resolves #14 Added Navbar and Footer

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <div className="navbar fixed top-0 z-20 bg-base-100 px-8 shadow-md">
+          <div className="navbar-start">
+            <Link href="/" className="bg-base-100 text-xl">
+              P-Layout
+            </Link>
+          </div>
+          <div className="navbar-end">
+            <div className="flex flex-row items-center gap-2">
+              <Link
+                href="/signup"
+                className="bg-primary-gradient hover:border-purple-900 hover:border-1 text-white btn border-base-100"
+              >
+                Sign Up
+              </Link>
+              <Link href="/login" className="btn border-gray-200 bg-base-100">
+                Log In
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
