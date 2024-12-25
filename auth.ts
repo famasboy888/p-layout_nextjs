91b6a751561ef7dc3684bff4f8796b9609910adc
famasboy888 <famasboy88@gmail.com>
2024-12-25 22:03:23 +0800

    Resolves #40; Added google provider verification

--- auth.ts
+++ auth.ts
@@ -6,31 +6,41 @@ import dbConnect from "~/utils/db/db";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   callbacks: {
-    async signIn({ user }) {
-      await dbConnect();
+    async signIn({ user, account }) {
+      if (account?.provider === "google") {
+        try {
+          await dbConnect();
 
-      // Check if the user already exists
-      const existingUser = await User.findOne({ email: user.email });
+          // Check if the user already exists
+          const existingUser = await User.findOne({ email: user.email });
 
-      if (!existingUser) {
-        await User.create({
-          // If the user doesn't exist, create a new user
-          email: user.email,
-          name: user.name,
-        });
+          if (!existingUser) {
+            await User.create({
+              // If the user doesn't exist, create a new user
+              email: user.email,
+              name: user.name,
+            });
+          }
+        } catch (error) {
+          console.error(error);
+        }
       }
 
       return true;
     },
     async session({ session }) {
-      const dbUser = await User.findOne({
-        email: session.user.email,
-      });
+      try {
+        const dbUser = await User.findOne({
+          email: session.user.email,
+        });
 
-      if (dbUser) {
-        session.user.id = dbUser._id.toString();
-        session.user.email = dbUser.email;
-        session.user.name = dbUser.name;
+        if (dbUser) {
+          session.user.id = dbUser._id.toString();
+          session.user.email = dbUser.email;
+          session.user.name = dbUser.name;
+        }
+      } catch (error) {
+        console.error(error);
       }
 
       return session;
