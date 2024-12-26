dd9353f66cf695cd3e7292e213eba497e96dae51
famasboy888 <famasboy88@gmail.com>
2024-12-26 21:03:33 +0800

    Resolves #55

--- src/app/dashboard/admin/[id]/page.tsx
+++ src/app/dashboard/admin/[id]/page.tsx
@@ -2,7 +2,7 @@ import { checkAuthorizationProtectedRoute } from "~/utils/checkAuthorization";
 
 export default async function page() {
   //Route protection
-  await checkAuthorizationProtectedRoute();
-  
+  await checkAuthorizationProtectedRoute("admin");
+
   return <div>Dashboard Admin</div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import LandingPage from "~/app/components/landingPage/LandingPage";
 export default async function Home() {
   return (
     //Test DaisyUI
-    <div>
-      <LandingPage />
-    </div>
+    <LandingPage />
   );
 }
