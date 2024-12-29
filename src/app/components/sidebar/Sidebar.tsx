79b5492e7a1c2836ae11af686995b904f4b101f9
famasboy888 <famasboy88@gmail.com>
2024-12-29 12:27:22 +0800

    Resolves #66

---
+++ src/app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,35 @@
+import SidebarMenu from "./components/SidebarMenu";
+
+export default function Sidebar() {
+  return (
+    <div className="drawer-start drawer">
+      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        {/* Page content here */}
+        <label htmlFor="my-drawer-4" className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-4"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <SidebarMenu />
+      </div>
+    </div>
+  );
+}
