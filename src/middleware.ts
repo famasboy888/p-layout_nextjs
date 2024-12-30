0e14120722c31667b5d68df70915162f3942b434
famasboy888 <famasboy88@gmail.com>
2024-12-30 15:31:12 +0800

    Resolves #44; Added Middleware routes and protection routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,27 @@
 import { type NextRequest, NextResponse } from "next/server";
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
-
+import { PUBLIC_ROUTES, ROOT } from "./middleware/route.middleware";
 const { auth } = NextAuth(authConfig);
 
 export async function middleware(req: NextRequest) {
+  const { nextUrl } = req;
+
   const session = await auth();
-  return NextResponse.redirect(new URL("/dummy", req.url));
+
+  const isAuthenticated = !!session?.user;
+
+  const isPublicRoute =
+    PUBLIC_ROUTES.find((route) => nextUrl.pathname.startsWith(route)) ??
+    nextUrl.pathname === ROOT;
+
+  console.log("Is this publicRoute", isPublicRoute, nextUrl.pathname);
+
+  if (!isAuthenticated && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 }
 
 export const config = {
-  matcher: ["/dummy"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
---
+++ src/middleware/route.middleware.ts
@@ -0,0 +1,11 @@
+export const ROOT = "/";
+export const LOGIN = "/login";
+export const REGISTER = "/register";
+export const UNAUTHORIZED = "/unauthorized";
+
+export const PUBLIC_ROUTES = [
+  "/login",
+  "/register",
+  "/unauthorized",
+  "/api/auth",
+];
