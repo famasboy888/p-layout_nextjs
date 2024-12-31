bd5771090fe901c80f13d6e31d1d6e1c8f7412f9
Kyle Yap <23441168+famasboy888@users.noreply.github.com>
2024-12-31 12:57:42 +0800

    Fixes #79 (#80)

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,9 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="footer footer-center bg-base-100 p-4 text-base-content border-gray-100 border-t-2">
+    <footer className="footer footer-center sticky top-[100vh] border-t-2 border-gray-100 bg-base-100 p-4 text-base-content">
       <aside>
         <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by P-Layout | BLUSH Flowers & Events.
+          Copyright © {new Date().getFullYear()} - All right reserved by
+          P-Layout | BLUSH Flowers & Events.
         </p>
       </aside>
     </footer>
