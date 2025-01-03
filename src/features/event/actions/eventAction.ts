cd3b6a6ded921148ed2c2df2d4b626183e1d508d
famasboy888 <famasboy88@gmail.com>
2025-01-04 01:14:44 +0800

    Resolves #88; successfull insert event

--- src/features/event/actions/eventAction.ts
+++ src/features/event/actions/eventAction.ts
@@ -1,8 +1,9 @@
 "use server";
 import { Types } from "mongoose";
 import { checkAuthorizationProtectedRoute } from "~/lib/auth/checkAuthorization";
-import { EventArraySchema } from "../dto/event.dto";
-import { getAllEventsByUserId } from "../repositories/event.repo";
+
+import { EventDisplayArraySchema, EventInsertSchema } from "../dto/event.dto";
+import { createEvent, getAllEventsByUserId } from "../repositories/event.repo";
 import { type EventDisplayDTO } from "../types/eventType";
 
 export const fetchEventOfUser = async () => {
@@ -13,7 +14,7 @@ export const fetchEventOfUser = async () => {
 
     const events = await getAllEventsByUserId(userId);
 
-    const mappedEventData = EventArraySchema.parse(
+    const mappedEventData = EventDisplayArraySchema.parse(
       events,
     ) as Partial<EventDisplayDTO>[];
 
@@ -24,21 +25,36 @@ export const fetchEventOfUser = async () => {
   }
 };
 
-function simulateApiCall(): Promise<string> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("API call completed");
-    }, 5000); // 20 seconds delay
-  });
-}
-
 export const addEvent = async (prevState: unknown, formData: FormData) => {
-
   try {
-    const result = await simulateApiCall();
-    return {
-      message: "Success",
-    };
+    const session = await checkAuthorizationProtectedRoute("coordinator");
+    const userId = new Types.ObjectId(session.user.id);
+
+    const eventData = EventInsertSchema.parse({
+      createdBy: userId,
+      withPermission: [userId],
+      eventName: formData.get("eventName") as string,
+      eventDesc: formData.get("eventDesc") as string,
+      eventType: formData.get("eventType") as string,
+      eventDate: new Date(formData.get("eventDate") as string),
+      eventCeremonyTime: formData.get("eventCeremonyTime") as string,
+      eventReceptionTime: formData.get("eventReceptionTime") as string,
+      eventCeremonyLoc: formData.get("eventCeremonyLoc") as string,
+      eventReceptionLoc: formData.get("eventReceptionLoc") as string,
+      eventStatus: "planning",
+    });
+
+    const res = await createEvent(eventData);
+
+    if (res) {
+      return {
+        message: "Success",
+      };
+    } else {
+      return {
+        message: "Failed",
+      };
+    }
   } catch (error) {
     console.error("Error during API call:", error);
     return {
--- src/features/event/dto/event.dto.ts
+++ src/features/event/dto/event.dto.ts
@@ -1,7 +1,7 @@
 import { Types } from "mongoose";
 import { z } from "zod";
 
-// Define a Zod schema for the user DTO
+// Define a Zod schema for the event DTO display
 const createEventSchema = () => {
   return z.object({
     _id: z.instanceof(Types.ObjectId).transform((objId) => objId.toString()), // Transform ObjectId to string
@@ -25,4 +25,19 @@ const createEventSchema = () => {
   });
 };
 
-export const EventArraySchema = z.array(createEventSchema());
+export const EventDisplayArraySchema = z.array(createEventSchema());
+
+// Define a Zod schema for the event DTO insert
+export const EventInsertSchema = z.object({
+  createdBy: z.instanceof(Types.ObjectId), // Assuming createdBy is a string
+  withPermission: z.array(z.instanceof(Types.ObjectId)), // Assuming withPermission is an array of strings
+  eventName: z.string(),
+  eventDesc: z.string().optional(),
+  eventType: z.string(),
+  eventDate: z.date(), // Assuming eventDate is a Date object
+  eventCeremonyTime: z.string().optional(),
+  eventReceptionTime: z.string().optional(),
+  eventCeremonyLoc: z.string().optional(),
+  eventReceptionLoc: z.string().optional(),
+  eventStatus: z.enum(["planning", "completed", "cancelled"]), // Assuming these are the possible statuses
+});
